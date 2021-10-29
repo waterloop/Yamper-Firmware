e0893faa6a51199acdc26c08a96245f709166292
Ryan Mah <rmah@uwaterloo.ca>
2021-10-28 20:43:42 -0600

    actually does what its supposed to

--- STM32F042K4Tx/Core/Src/application_code.c
+++ STM32F042K4Tx/Core/Src/application_code.c
@@ -18,58 +18,27 @@ float compute_rpm(int n) {
     return (2*M_PI*R*n)/(N*SAMPLING_PERIOD_S);
 }
 
-float* velocity;
-uint8_t bytes_array[4];
-CAN_TxHeaderTypeDef TxHeader;
-extern uint32_t TxMailbox;
-
 int app_main() {
     if (CANBus_init(&hcan) != HAL_OK) { Error_Handler(); }
-    // if (CANBus_subscribe(POD_SPEED) != HAL_OK) { Error_Handler(); }
-    if (CANBus_subscribe_all() != HAL_OK) { Error_Handler(); }
 
-    // CANFrame tx_frame = CANFrame_init(POD_SPEED.id);
-    // CANFrame_set_field(&tx_frame, POD_SPEED, FLOAT_TO_UINT(69.420));
+    init_timer();
+    __HAL_TIM_Base_Start_IT(&htim14);
 
-    // hcan.Instance->MCR |= (1 << 16);
-
-    CANFrame tx_frame;
+    int count;
+    float rpm;
+    CANFrame tx_frame = CANFrame_init(POD_SPEED.id);
     while (1) {
-        // if (CANBus_put_frame(&tx_frame) != HAL_OK) { Error_Handler(); }
+        count = TIM3->CNT;
+        rpm = compute_rpm(count);
 
-        if (!Queue_empty(&RX_QUEUE)) {
-            CANFrame rx_frame = CANBus_get_frame();
-            
-            tx_frame = CANFrame_init(rx_frame.id);
-            for (uint8_t i = 0; i < 8; i++) {
-                tx_frame.pld[i] = rx_frame.pld[i];
-            }
-            if (CANBus_put_frame(&tx_frame) != HAL_OK) { Error_Handler(); }
+        CANFrame_set_field(&tx_frame, POD_SPEED, FLOAT_TO_UINT(rpm));
+        if (CANBus_put_frame(&tx_frame) != HAL_OK) {
+            Error_Handler();
         }
-        HAL_Delay(5);
+
+        HAL_Delay(100);
     }
 
     return 0;
 }
-
-// int app_main() {
-//     init_timer();
-// 
-//     HAL_CAN_Start(&hcan);
-//     HAL_TIM_Base_Start_IT(&htim14);
-//     __HAL_TIM_SET_COUNTER(&htim14, 0);
-// 
-//     while (1) {
-//         int count = TIM3->CNT;
-//         *velocity = compute_rpm(count);
-//         add_data(velocity, bytes_array, 32, 1, 0);
-//         HAL_CAN_AddTxMessage(&hcan, &TxHeader, bytes_array, &TxMailbox);
-//         while (HAL_CAN_IsTxMessagePending(&hcan, TxMailbox));
-// 
-//         HAL_Delay(100);
-//     }
-// 
-//     return 0;
-// }
-
 
